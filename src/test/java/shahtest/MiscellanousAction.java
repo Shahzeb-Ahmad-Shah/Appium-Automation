2a0992a2a5565451e2df44cce8bd7f70a87ecfcf
Shahzeb402 <shahzeb.shah@teo-intl.com>
2025-10-01 14:42:59 +0500

    Added Rotation of screen, ClipBoard test & Pressing keys

---
+++ src/test/java/shahtest/MiscellanousAction.java
@@ -0,0 +1,52 @@
+package shahtest;
+
+import io.appium.java_client.android.nativekey.KeyEvent;
+import java.io.File;
+
+import java.net.MalformedURLException;
+
+import java.net.URI;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.windows.WindowsDriver;
+
+public class MiscellanousAction extends BaseTest {
+	
+	@Test
+	public void miscAction() throws MalformedURLException
+	{
+		//Actual Automation
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+		driver.findElement(By.id("android:id/checkbox")).click();
+		DeviceRotation landscape = new DeviceRotation(0, 0, 90);
+		driver.rotate(landscape);
+		driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+		String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+		Assert.assertEquals(alertTitle, "WiFi settings");
+		driver.setClipboardText("TEO Guest");
+		driver.findElement(By.id("android:id/edit")).sendKeys(driver.getClipboardText());
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click(); 
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+		driver.pressKey(new KeyEvent(AndroidKey.HOME));
+		
+		//set wifi name
+		
+	}
+
+}
